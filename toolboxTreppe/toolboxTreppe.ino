78d72798e6a6f8e12650afb20d8ac87bbbe5df6c
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2018-12-13 20:27:20 +0100

    noch ein paar Änderungen + Anmerkungen (#2)
    * changed variable name
    
    lokale Variablen sollten nicht den gleichen Namen wie globale haben (auch wenn das in diesem Fall trotzdem funktioniert)
    
    * changed variable type
    
    removed some complexity
    
    * removed unused if
    
    this if could never be true
    and changed variable name from i to returnValue, since i is a variablename you might use for a counter but it does not really give a good description of what it´s used for
    
    gegenstrecke is always between 0 and a value smaller than 255 and is passed as uint8_t so it can be changed to unit8_t same with xB and xA they get passed as uint8_t so they have to be in this range anyways
    
    * change order
    
    changed the order to prevent xA from overflowing
    
    hue gets now incremented outside the CHSV function call
    
    * made all variables local
    
    replaced global definitions by defines
    
    * removed #define that was double
    
    * added definitions for functions in top
    
    now using const reference for xY and XY safe aswell
    returnvalue will not be changed -> const
    
    * changed debugfunction
    
    * fixed the debug Function
    
    * fixed a couple syntax issues and added sprintf
    
    * fixed end of file
    
    * forgot to change a name
    
    * forgot to change the parameters in the declaration
    
    * fixed char array

--- toolboxTreppe/toolboxTreppe.ino
+++ toolboxTreppe/toolboxTreppe.ino
@@ -1,48 +1,38 @@
 #include <FastLED.h>
+#include <stdarg.h>
 
 #define LED_PIN  6
 
 #define COLOR_ORDER GRB
-#define CHIPSET     WS2811
+#define CHIPSET WS2811
 
 #define BRIGHTNESS 255
 
 // Breite und Höhe
-const uint8_t kMatrixWidth = 11; //----------.Breite
-const uint8_t kMatrixHeight = 17; //----------Höhe
-
-#define NUM_LEDS kMatrixWidth*kMatrixHeight;
+#define kMatrixWidth 11 //----------.Breite
+#define kMatrixHeight 17 //----------Höhe
 
 //Button Variablen
-const int buttonPin1 = 10;//Switch Oben und Mitte 
-const int buttonPin2 = 11; //Switch unten und Mitte
-const int buttonPin3 = 9; //Rot unten
-const int buttonPin4 = 8; // Rot oben
+#define buttonPin1 10//Switch Oben und Mitte 
+#define buttonPin2 11 //Switch unten und Mitte
+#define buttonPin3 9 //Rot unten
+#define buttonPin4 8 // Rot oben
 
 //Zeit Variablen
-long xTime = 10; //---------------------------Intervall 1
-long onewayTime = 42; //----------------------Intervall 2
-
-//Nicht verändern
-long xMillis = 0;
-long onewayMillis = 0;
-//LED Variablen
-int y = 0;
-int x = 0;
-int xA = 0;
-int xB = 0;
-int gegenstrecke = kMatrixHeight-1;
-static uint8_t hue;
-unsigned long currentMillis = millis();
+#define xTime 10 //---------------------------Intervall 1
+#define onewayTime 42 //----------------------Intervall 2
 
 //------------------------------------------------------
 
 // Param for different pixel layouts
-const bool    kMatrixSerpentineLayout = true;
+#define kMatrixSerpentineLayout true
 #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
 CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
 CRGB* const leds( leds_plus_safety_pixel + 1);
 
+void animation();
+void debugPrint(const char* output, ...);
+const uint16_t XY(const uint8_t& x, const uint8_t& y);
 
 //--------------------------------SETUP ------------------------
 void setup() {
@@ -53,99 +43,74 @@ void setup() {
   pinMode(buttonPin3, INPUT_PULLUP);
   pinMode(buttonPin4, INPUT_PULLUP);
   Serial.begin(9600);
-  Serial.println("Hello World!");
+  debugPrint("Hello World!");
 }
-
-
-uint16_t XYsafe( const uint8_t x, const uint8_t y)
-{
-  if( x >= kMatrixWidth) return -1;
-  if( y >= kMatrixHeight) return -1;
-  return XY(x,y);
-}
-
 
 //----------------------------------LOOP -------------------
 void loop() 
-{
+{ 
   //Wenn Schalter oben ist, starte Animation.
   if (digitalRead(buttonPin1) == HIGH) {
-    //Wenn der Schalter in der mitte ist, aktiviere debugfunktion
-    if (digitalRead(buttonPin2) == HIGH) {
-      debug();
-    }
-    animation1();
+    animation();
   } 
   //LEDs ausschalten, wenn der switch unten ist (Pinkontakt 2+3)
-  else if ((digitalRead(buttonPin1) == LOW) && (digitalRead(buttonPin2) == HIGH)) {
+  else if (digitalRead(buttonPin2) == HIGH) {
     powerOff();
   }
 }
 
-
-
-
 //-------------------------------Animationsfunktion ------------
-void animation1() {
-  currentMillis = millis();
+void animation() {
+  static uint8_t xA;
+  static uint8_t xB;
+  static unsigned long xMillis;
+  static long onewayMillis;
+  static uint8_t hue;
+  static uint8_t y;
+  static uint8_t x;
+  static uint8_t gegenstrecke = kMatrixHeight-1;
+  debugPrint("x: %d   y: %d   gegenstrecke: %d", x, y, gegenstrecke);
+  
+  unsigned long currentMillis = millis();
   if (currentMillis - xMillis > xTime) {
-      xMillis = currentMillis;
-      ++x;
-      xA = 5 - x;
-      xB = 5 + x;
-      if (x < 0) {
-        x = 0;
-      }else if (x > 5) {
-        x = 0;
-        ++y;
-        //hue++;
-        fadeToBlackBy(leds, NUM_LEDS, 20);
-        if (y > kMatrixHeight-1 || y < 0) {
-          y = 0;
-        }
+    xMillis = currentMillis;
+    ++x;
+    if (x > 5) {
+      x = 0;
+      ++y;
+      fadeToBlackBy(leds, NUM_LEDS, 20);
+      if (y > kMatrixHeight-1) {
+        y = 0;
       }
     }
-    if (currentMillis - onewayMillis > onewayTime) {
-      onewayMillis = currentMillis;
+    xA = 5 - x;
+    xB = 5 + x;
+  }
+  if (currentMillis - onewayMillis > onewayTime) {
+    onewayMillis = currentMillis;
+    if (gegenstrecke == 0) {
+      gegenstrecke = kMatrixHeight-1;
+    } else {
       --gegenstrecke;
-      if (gegenstrecke < 0 || gegenstrecke > kMatrixHeight-1) {
-        gegenstrecke = kMatrixHeight-1;
-      }
     }
+  }
     
-    leds[ XY(xA, y)] = CHSV( hue++, 255, 255 );
-    leds[ XY(xB, y)] = CHSV( hue, 255, 255 );
-    leds[ XY(5, gegenstrecke)] = CHSV( hue, 255, 255 );
-    FastLED.show();
-    if (hue > 255) {
-      hue = 0;
-    }
+  ++hue;
+  leds[ XY(xA, y)] = CHSV( hue, 255, 255 );
+  leds[ XY(xB, y)] = CHSV( hue, 255, 255 );
+  leds[ XY(5, gegenstrecke)] = CHSV( hue, 255, 255 );
+  FastLED.show();
 }
 //----------------------------- Debug Funktion ----------------
-void debug() {
-  Serial.print(y);
-  Serial.print("   ");
-  Serial.print(x);
-  Serial.print("   ");
-  Serial.print(gegenstrecke);
-  Serial.print("   ");
-  if ((digitalRead(buttonPin1) == HIGH) && (digitalRead(buttonPin2) == LOW)) {
-    Serial.print("Oben ");
+
+void debugPrint(const char* output, ...) {
+  va_list args;
+  char buffer[1024];
+  if (digitalRead(buttonPin1) && digitalRead(buttonPin2) && (sprintf (buffer, output, args) >= 0) ) {
+    Serial.println(buffer);
   }
-  if ((digitalRead(buttonPin1) == HIGH) && (digitalRead(buttonPin2) == HIGH)) {
-    Serial.print("Mitte ");
-  }
-  if ((digitalRead(buttonPin1) == LOW) && (digitalRead(buttonPin2) == HIGH)) {
-    Serial.print("Unten ");
-  }
-  if (digitalRead(buttonPin3) == HIGH) {
-    Serial.print("Knopf 3");
-  }
-  if (digitalRead(buttonPin4) == LOW) {
-    Serial.print("Knopf 4");
-  }
-  Serial.println(" ");
 }
+
 //----------------------------------Power Off -------------
 void powerOff() {
   FastLED.clear();
@@ -153,52 +118,16 @@ void powerOff() {
 }
 
 //------------------------ Matrix definitionen ----------
-
-uint16_t XY(const uint8_t x, const uint8_t y) {
-  uint16_t i;
+const uint16_t XY(const uint8_t& x, const uint8_t& y) {
+  uint16_t returnValue = y * kMatrixWidth;
   
-  if( kMatrixSerpentineLayout == false) {
-    i = (y * kMatrixWidth) + x;
+  if( kMatrixSerpentineLayout && y & 0x01) {
+    // Odd rows run backwards
+    returnValue += kMatrixWidth - 1 - x;
   } else {
-    if( y & 0x01) {
-      // Odd rows run backwards
-      uint8_t reverseX = (kMatrixWidth - 1) - x;
-      i = (y * kMatrixWidth) + reverseX;
-    } else {
-      // Even rows run forwards
-      i = (y * kMatrixWidth) + x;
-    }
+    // Even rows run forwards
+    returnValue += x;
   }
-  
-  return i;
+  return returnValue;
 }
 
-//---------------------Example Code ------------------------
-/*
-void loop()
-{
-    uint32_t ms = millis();
-    int32_t yHueDelta32 = ((int32_t)cos16( ms * (27/1) ) * (350 / kMatrixWidth));
-    int32_t xHueDelta32 = ((int32_t)cos16( ms * (39/1) ) * (310 / kMatrixHeight));
-    DrawOneFrame( ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
-    if( ms < 5000 ) {
-      FastLED.setBrightness( scale8( BRIGHTNESS, (ms * 256) / 5000));
-    } else {
-      FastLED.setBrightness(BRIGHTNESS);
-    }
-    FastLED.show();
-}
-
-void DrawOneFrame( byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
-  byte lineStartHue = startHue8;
-  for( byte y = 0; y < kMatrixHeight; y++) {
-    lineStartHue += yHueDelta8;
-    byte pixelHue = lineStartHue;      
-    for( byte x = 0; x < kMatrixWidth; x++) {
-      pixelHue += xHueDelta8;
-      leds[ XY(x, y)]  = CHSV( pixelHue, 255, 255);
-    }
-  }
-}
-*/
